8344c9e16831e327655b7831e5525dcfb9d212f0
Mudit Raj Gupta <mudit.raaj.gupta@gmail.com>
2015-10-17 11:40:41 +0530

    Deepcopy bug

--- plotvals.py
+++ plotvals.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from copy import deepcopy
 import wave
 import struct
 import sys
@@ -17,6 +18,16 @@ from pylab import *
 import json
 
 final_val = []
+final_val_slice = []
+
+VIOLET = "#ee82ee"
+BLUE = "#0000ff"
+GREEN = "#008000"
+YELLOW = "#ffff00"
+ORANGE = "#ffa500"
+RED = "#ff0000"
+
+color = [VIOLET, BLUE, GREEN, YELLOW, ORANGE, RED]
 
 def mp3_to_wave(mp3_filename):
 	# convert mp3, read wav
@@ -24,23 +35,59 @@ def mp3_to_wave(mp3_filename):
 	check_call(['avconv', '-i', mp3_filename, wname])
 	return wname
 
-def get_color_hash(intensity):
-	RED = "#ff0000"
-	GREEN = "#008000"
-	return RED
+def get_color_hash(max_pos):
+	count = 0
+	inc = 2
+	while max_pos > inc:
+		inc += 2
+		count += 1
+	return color[count]
 
 def process_final_val(number_of_peers):
 	max_amp = 0
 	max_amp = np.amax(final_val)
-	final_val_norm = final_val/max_amp
-	max_val = np.amax(final_val_norm)
-	response_array = [[] for i in range(number_of_peers)]
-	for time_slice in final_val_norm:
-		for peer in range(0, len(time_slice)) :
+	#print final_val
+	#raw_input()
+	print type(final_val)
+	print final_val
+	final_val_norm = []
+	#raw_input()
+	for ts in final_val :
+		tmp = []
+		for i in ts : 
+			tmp.append(i/max_amp)
+		final_val_norm.append(deepcopy(tmp))
+	print final_val_norm
+
+	#final_val_norm = final_val/max_amp
+	#print final_val_norm
+	#raw_input()
+	#max_val = np.amax(final_val_norm)
+	#response_array = [[] for i in range(number_of_peers)]
+	response_array = []
+	for i in final_val_norm:
+		for time_slice in i:
+			print time_slice
+#			raw_input()
+	#	print time_slice
+	#	raw_input()
+			print time_slice
+			if time_slice == [] :
+				continue
+			max_amp_val_ts = max(time_slice)
+	#	print max_amp_val_ts
+	#	raw_input()
+			max_pos = [i for i, j in enumerate(time_slice) if j == max_amp_val_ts]
+	#	print max_pos
+	#	raw_input()
+		#for peer in range(0, len(time_slice)) :
 			resp = {}
-			resp["Intensity"] = time_slice[peer]
-			resp["Color"] = get_color_hash(resp["Intensity"])
-	  		response_array[peer].append(resp)
+			resp["Intensity"] = max_amp_val_ts
+			resp["Color"] = min(max_pos)
+	#	print resp 
+	#	raw_input()
+			response_array.append(resp)
+	  	#response_array[peer].append(resp)
 	return response_array
 
 
@@ -50,15 +97,15 @@ def compute(filename, number_of_peers):
 	wave_file = wave.open(wave_file_name, 'r')
 	data_size = wave_file.getnframes()
 	sample_rate = wave_file.getframerate()
-	print "Sample rate: %s" % sample_rate
+	#print "Sample rate: %s" % sample_rate
 	sample_width = wave_file.getsampwidth()
 	duration = data_size / float(sample_rate)
 
 	# Read in sample data
 	sound_data = wave_file.readframes(data_size)
 
-	print type(sound_data)
-	print len(sound_data)
+	#print type(sound_data)
+	#print len(sound_data)
 
 	# Close the file, as we don't need it any more
 	wave_file.close()
@@ -95,7 +142,7 @@ def compute(filename, number_of_peers):
 	sample_size = fourier_width_index
 	freq = sample_rate / sample_size * np.arange(sample_size)
 
-	x_axis = range(0, 12)
+	#x_axis = range(0, 12)
 
 	def getBandWidth():
 		return (2.0/sample_size) * (sample_rate / 2.0)
@@ -114,6 +161,17 @@ def compute(filename, number_of_peers):
 
 	def average_fft_bands(fft_array):
 		num_bands = number_of_peers # The number of frequency bands (12 = 1 octave)
+		#final_val += fft_averages
+		#print "-----------------------------------------------------------------------------"
+		#print "before"
+		#print final_val_slice 
+		#raw_input()
+		#print fft_averages
+		#raw_input()
+		#print "after"
+		final_val.append(deepcopy(fft_averages))
+		#print final_val_slice
+		#raw_input()
 		del fft_averages[:]
 		for band in range(0, num_bands):
 			avg = 0.0
@@ -130,6 +188,8 @@ def compute(filename, number_of_peers):
 
 			avg /= (hiBound - lowBound + 1)
 			fft_averages.append(avg)
+		#print fft_averages
+		#raw_input()
 		return fft_averages
 
 	for offset in range(0, total_transforms):
@@ -143,8 +203,13 @@ def compute(filename, number_of_peers):
 		# Normalise the data a second time, to make numbers sensible
 		fft_data *= ((2**.5)/sample_size)
 		#plt.ylim(0, 1000)
-		averages = average_fft_bands(fft_data)
-		final_val.append(averages)
+		average_fft_bands(fft_data)
+		#print averages
+		#raw_input()
+		#final_val.append(averages)
+		#averages = []
+		#print final_val
+		#raw_input()
 		#y_axis = fft_averages
 		"""Stuff for bar graph"""
 		#width = 0.35
@@ -156,7 +221,7 @@ def compute(filename, number_of_peers):
 		"""End bar graph stuff"""
 		#filename = str('frame_%05d' % offset) + '.png'
 		#plt.savefig(filename, dpi=100)
-	print final_val
+	#print final_val
 	return process_final_val(number_of_peers)
 
 if __name__ == '__main__':
